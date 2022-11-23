a57ef77543f51a2b209a6151d1ecd1c2fc5e96cd
Cameleon00722 <45.worker@protonmail.com>
2022-11-23 11:29:07 +0100

    ajout mélange

--- shared/ontest.py
+++ shared/ontest.py
@@ -1,4 +1,6 @@
 import asyncio
+import random
+
 
 
 class table:
@@ -30,10 +32,327 @@ class table:
     def affiche(self):
         print("Nom de la table =", self.nom, "liste des joueurs =", self.lst_table, "temps =", self.temps)
 
+class Cartes:
+    def __init__(self, suite, valeur, carte_valeur):
+        # Suite des carte tel que coeur pique ...
+        self.suite = suite
+
+        # Représentation des cartes Roi, As ...
+        self.valeur = valeur
+
+        # Valeur des cartes
+        self.carte_valeur = carte_valeur
+
+
+class Joueur:
+    def __init__(self, name):
+        self.mainJ = []
+
+        self.name = name
+
+        self.score = 0
+
+    def getMainJ(self):
+        return self.mainJ
+
+    def getScore(self):
+        return self.score
+
+    def setMainJ(self, lst_main):
+        self.mainJ = lst_main
 
 # liste des tables de jeux , sous tab de la classe = joueur
 tableaudetable = []
 
+# Affiché la carte
+def print_cartes(cartes, dissimuler):
+    s = ""
+    for carte in cartes:
+        s = s + "\t ________________"
+    if dissimuler:
+        s += "\t ________________"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|                |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        if carte.valeur == '10':
+            s = s + "\t|  {}            |".format(carte.valeur)
+        else:
+            s = s + "\t|  {}             |".format(carte.valeur)
+    if dissimuler:
+        s += "\t|                |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|      * *       |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|    *     *     |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|   *       *    |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|   *       *    |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|       {}        |".format(carte.suite)
+    if dissimuler:
+        s += "\t|          *     |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|         *      |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|        *       |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|                |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|                |"
+    if dissimuler:
+        s += "\t|                |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        if carte.valeur == '10':
+            s = s + "\t|            {}  |".format(carte.valeur)
+        else:
+            s = s + "\t|            {}   |".format(carte.valeur)
+    if dissimuler:
+        s += "\t|        *       |"
+    print(s)
+
+    s = ""
+    for carte in cartes:
+        s = s + "\t|________________|"
+    if dissimuler:
+        s += "\t|________________|"
+    print(s)
+
+    print()
+
+
+# Game blackjack
+def blackjack(deck):
+    # Cartes du joueur et dealeur
+
+    joueur = Joueur("test")
+
+    joueur_cartes = joueur.getMainJ()
+    dealer_cards = []
+
+    # Score du joueur et dealer
+    joueur_score = joueur.getScore()
+    dealer_score = 0
+
+    while len(joueur_cartes) < 2:
+
+        joueur_carte = random.choice(deck)
+        joueur_cartes.append(joueur_carte)
+        deck.remove(joueur_carte)
+
+        joueur_score += joueur_carte.carte_valeur
+
+        if len(joueur_cartes) == 2:
+            if joueur_cartes[0].carte_valeur == 11 and joueur_cartes[1].carte_valeur == 11:
+                joueur_cartes[0].carte_valeur = 1
+                joueur_score -= 10
+
+        print("CARTES JOUEUR: ")
+        print_cartes(joueur_cartes, False)
+        print("SCORE JOUEUR = ", joueur_score)
+
+        dealer_card = random.choice(deck)
+        dealer_cards.append(dealer_card)
+        deck.remove(dealer_card)
+
+        # Update score
+        dealer_score += dealer_card.carte_valeur
+
+        print("CARTES CROUPIER: ")
+        if len(dealer_cards) == 1:
+            print_cartes(dealer_cards, False)
+            print("CROUPIER SCORE = ", dealer_score)
+        else:
+            print_cartes(dealer_cards[:-1], True)
+            print("CROUPIER SCORE = ", dealer_score - dealer_cards[-1].carte_valeur)
+
+        if len(dealer_cards) == 2:
+            if dealer_cards[0].carte_valeur == 11 and dealer_cards[1].carte_valeur == 11:
+                dealer_cards[1].carte_valeur = 1
+                dealer_score -= 10
+
+    # Joueur a le blackjack
+    if joueur_score == 21:
+        print("Le joueur remporte")
+        quit()
+
+    # Print dealer et joueur cartes
+    print("CARTES CROUPIER: ")
+    print_cartes(dealer_cards[:-1], True)
+    print("CROUPIER SCORE = ", dealer_score - dealer_cards[-1].carte_valeur)
+
+    print()
+
+    print("CARTES JOUEUR: ")
+    print_cartes(joueur_cartes, False)
+    print("SCORE JOUEUR = ", joueur_score)
+
+    while joueur_score < 21:
+        choice = input("Entrez P pour Prendre ou R pour Rester : ")
+
+        if choice.upper() != 'P' and choice.upper() != 'R':
+            print("erreur input merci de recommencer")
+
+        # Le joueur prend
+        if choice.upper() == 'P':
+
+            joueur_carte = random.choice(deck)
+            joueur_cartes.append(joueur_carte)
+            deck.remove(joueur_carte)
+
+            # Update joueur score
+            joueur_score += joueur_carte.carte_valeur
+
+            c = 0
+            while joueur_score > 21 and c < len(joueur_cartes):
+                if joueur_cartes[c].carte_valeur == 11:
+                    joueur_cartes[c].carte_valeur = 1
+                    joueur_score -= 10
+                    c += 1
+                else:
+                    c += 1
+
+            # Print joueur et dealer cartes
+            print("CARTES CROUPIER: ")
+            print_cartes(dealer_cards[:-1], True)
+            print("CROUPIER SCORE = ", dealer_score - dealer_cards[-1].carte_valeur)
+
+            print()
+
+            print("CARTES JOUEUR: ")
+            print_cartes(joueur_cartes, False)
+            print("SCORE JOUEUR = ", joueur_score)
+
+        # Si joueur reste
+        if choice.upper() == 'R':
+            break
+
+    # Print joueur et delaer cartes
+    print("CARTES JOUEUR: ")
+    print_cartes(joueur_cartes, False)
+    print("SCORE JOUEUR = ", joueur_score)
+
+    print()
+    print("LE CROUPIER REVELE LES CARTES....")
+
+    print("CARTES CROUPIER: ")
+    print_cartes(dealer_cards, False)
+    print("CROUPIER SCORE = ", dealer_score)
+
+    # Check si le joueur a le blackjack
+    if joueur_score == 21:
+        print("LE JOUEUR A LE BLACKJACK")
+        quit()
+
+    # Check si le joueur depasse
+    if joueur_score > 21:
+        print("LE JOUEUR A DEPASSER!!! PERDU!!!")
+        quit()
+
+    while dealer_score < 17:
+
+        print("LE CROUPIER DECIDE DE PRENDRE.....")
+
+        dealer_card = random.choice(deck)
+        dealer_cards.append(dealer_card)
+        deck.remove(dealer_card)
+
+        dealer_score += dealer_card.carte_valeur
+
+        # Update score
+        c = 0
+        while dealer_score > 21 and c < len(dealer_cards):
+            if dealer_cards[c].carte_valeur == 11:
+                dealer_cards[c].carte_valeur = 1
+                dealer_score -= 10
+                c += 1
+            else:
+                c += 1
+
+        # print joueur et dealer cartes
+        print("CARTES JOUEUR: ")
+        print_cartes(joueur_cartes, False)
+        print("SCORE JOUEUR = ", joueur_score)
+
+        print()
+
+        print("CARTES CROUPIER: ")
+        print_cartes(dealer_cards, False)
+        print("CROUPIER SCORE = ", dealer_score)
+
+    # Dealer depasse
+    if dealer_score > 21:
+        print("LE CROUPIER A DEPASSER!!! VOUS AVEZ GAGNE!!!")
+        quit()
+
+        # Le dealer a le blackjack
+    if dealer_score == 21:
+        print("LE CROUPIER A LE BLACKJACK!!! LE JOUEUR A PERDUE")
+        quit()
+
+    # Match nul
+    if dealer_score == joueur_score:
+        print("MATCH NUL!!!!")
+
+    # Joueur Wins
+    elif joueur_score > dealer_score:
+        print("LE JOUEUR A GAGNE!!!")
+
+        # Dealer Wins
+    else:
+        print("LE CROUPIER A GAGNE!!!")
 
 async def croupier_request(reader, writer):
     mess = "Bienvenue\n"
@@ -77,38 +396,42 @@ async def joueur_request(reader, writer):
                     print(i.getTemps())
                     await asyncio.sleep(1)
                     i.setTemps(i.getTemps() - 1)
-
+            mess2 = "Connexion a la table réussi!\n"
+            writer.write(mess2.encode())
+            mess2 = ".\n"
+            writer.write(mess2.encode())
 
     while True:
-        data = await reader.readline()
+        data = await reader.read(256)
+        message = data.decode()
 
         if reader.at_eof() :
-            print(f"Socket closed by user {addr}")
-            data = b"quit"
-        message = data.decode().strip()
-
+           print(f"Socket closed by user {joueur}")
+           data = b"quit"
+           message = data.decode().strip()
         if message == "quit":
-            message = f"User {addr} leave the server."
+            message = f"User {joueur} leave the server."
             print(message)
-            await forward(writer, "Server", message)
-            users.remove(writer)
+            #await forward(writer, "Server", message)
+            #users.remove(writer)
             writer.close()
             break
 
         if message == "MORE 1":
-            message = f"utilisateur {addr} prend une carte."
+            message = f"utilisateur {joueur} prend une carte."
             print(message)
-            await forward(writer, "Server", message)
+            #await forward(writer, "Server", message)
             #code à implémenté
 
         if message == "MORE 0":
-            message = f"utilisateur {addr} ne prend pas de carte."
+            message = f"utilisateur {joueur} ne prend pas de carte."
             print(message)
-            await forward(writer, "Server", message)
+            #await forward(writer, "Server", message)
             #code à implémenté
 
         print(message)
-        await forward(writer, addr, message)
+        #await forward(writer, addr, message)
+
 
 
 async def server():
